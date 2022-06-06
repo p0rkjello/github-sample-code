41beccd0684286e678e13962ded9ec67e2d503f5
Andrew Bounds <392383+p0rkjello@users.noreply.github.com>
2022-06-06 15:06:46 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "myawsserver" {
+  ami = "ami-0603cbe34fd08cb81"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "bounds-cloud-vm"
+    Env = "Dev"
+  }
+}
+
+output "myawsserver-ip" {
+  value = "${aws_instance.myawsserver.public_ip}"
+}
