b09cadc29f549bcbfee6cc21c65c16f18973034b
Andrew Bounds <392383+p0rkjello@users.noreply.github.com>
2022-06-06 15:56:20 -0400

    emptyr file

--- main.tf
+++ main.tf
@@ -1,17 +1,1 @@
-provider "aws" {
-  region = "us-east-2"
-}
 
-resource "aws_instance" "myawsserver" {
-  ami = "ami-0603cbe34fd08cb81"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "bounds-git-testing"
-    Env = "Test"
-  }
-}
-
-output "myawsserver-ip" {
-  value = "${aws_instance.myawsserver.public_ip}"
-}
